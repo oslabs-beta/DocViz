7380fdb76d1c6e3a09b282ff641285f6706dea33
KaishaerMahemuti <k.mahemuti@gmail.com>
2025-01-17 15:44:04 -0500

    deleted routes/index.js and modified globalErrorHander in middleware/errorhandler.js

--- server/app.js
+++ server/app.js
@@ -3,20 +3,23 @@ const path = require('path');
 const containerRoutes = require('./routes/containers');
 const indexRoutes = require('./routes/index');
 const requestLogger = require('./middlewares/requestLogger');
-const errorHandler = require('./middlewares/errorHandler');
+const globalErrorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
+
 // Middleware
 app.use(express.json());
+// logging req method and url
 app.use(requestLogger);
+// Serves an entire directory of static files
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Routers
 app.use('/api/containers', containerRoutes);
 app.use('/', indexRoutes);
 
-// Error Handling Middleware
-app.use(errorHandler);
+// Global Error Handler
+app.use(globalErrorHandler);
 
 module.exports = app;
--- server/middlewares/errorHandler.js
+++
@@ -1,4 +0,0 @@
-module.exports = function errorHandler(err, req, res, next) {
-    console.error(err.stack); // Log the error stack
-    res.status(500).send('Something went wrong!'); // Send a 500 response
-};
