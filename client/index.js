c222da9a1ca12594c4ff7b5abcb4e436a6876fdc
SierraR-AI <elevatewithsierra@gmail.com>
2025-01-17 12:10:15 -0500

    FrontEndBoilerPlate

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <h1>Welcome to DocViz!</h1>;
+
+// Create a root and render the App component
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
