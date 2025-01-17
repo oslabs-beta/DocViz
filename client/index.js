e1e2e1b4ab97df730d7b5bd7a0ed3681e1197409
SierraR-AI <elevatewithsierra@gmail.com>
2025-01-17 17:16:25 -0500

    Push Updates

--- client/index.js
+++ client/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './src/App';
 
-const App = () => <h1>Welcome to DocViz!</h1>;
+// const App = () => <h1>Welcome to DocViz!</h1>;
 
 // Create a root and render the App component
 const root = ReactDOM.createRoot(document.getElementById('root'));
